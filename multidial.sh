5a497dd2a99df315375eb585a27684e5cc6bdb3a
Yujie Xia <xiayjchn@gmail.com>
2018-04-14 21:49:53 +0800

    add dial_helper

--- multidial.sh
+++ multidial.sh
@@ -10,31 +10,35 @@
 # Defaults
 VTH="vth"
 ETH="eth0"
-USER="scu@edu"
-PASSWORD=""
 CONNECT_POLL=1
 CONNECT_TIMEOUT=10
-IPV6_PREFIX="2001:250:2003:2010:200:5efe"
+DATA_DIR='/tmp/multidial'
+
+# Available dial method
+METHODS=('pppoe' 'dhcp' 'static')
+
+# PPPoE configuration
+USER="scu@edu"
+PASSWORD=""
+
+# ISATAP configuration
 REMOTE_ROUTER="202.115.39.98"
-START_VTH_ID=1
-START_ADDRESS="121.48.228.10"
+IPV6_PREFIX="2001:250:2003:2010:200:5efe"
+
+# Static IP address
+ADDRESS="121.48.228.10"
 GATEWAY="121.48.228.1"
 NETMASK=24
-
-ME=$(basename "$0")
-# Must be root
-if [ "$(/usr/bin/id -u)" != 0 ]; then
-    echo "$ME: You must be root to run this script" >&2
-    exit 1
-fi
-
-usage() {
-    echo
-}
 
 error() {
     echo "error: $*" >&2
 }
+
+# Must be root
+if [ "$(/usr/bin/id -u)" != 0 ]; then
+    error "You must be root to run this script"
+    exit 1
+fi
 
 # Output IP addresses on the interface
 # Args: [ifname] [--ipv4|ipv4]
@@ -56,7 +60,9 @@ get_ip() {
 # e.g. create_virtual_interface vth0
 create_virtual_interface() {
     local ifname=$1
+    [ -f "$DATA_DIR/$ifname" ] && rm -f "$DATA_DIR/$ifname" 2>/dev/null
     ip link add link "$ETH" name "$ifname" type macvlan >/dev/null 2>&1 || return 1
+    echo >>"$DATA_DIR/$ifname"
 }
 
 # Remove virtual interface
@@ -65,6 +71,7 @@ create_virtual_interface() {
 remove_virtual_interface() {
     local ifname=$1
     ip link show "$ifname" >/dev/null 2>&1 || return
+    [ -f "$DATA_DIR/$ifname" ] && rm -f "$DATA_DIR/$ifname" 2>/dev/null
     if ip link show "$ifname" | grep "$ifname@$ETH" >/dev/null; then
         ip link set "$ifname" down
         ip link del "$ifname"
@@ -80,7 +87,7 @@ build_isatap_tunnel() {
     local ipv4
     ipv4=$(get_ip "$ifname" --ipv4)
     [ -z "$ipv4" ] && return 1
-    ip link show | grep "$isatap_ifname" >/dev/null && return 1
+    ip link show "$isatap_ifname" >/dev/null 2>&1 && return 1
     ip tunnel add "$isatap_ifname" mode sit remote "$REMOTE_ROUTER" local "$ipv4"
     ip link set dev "$isatap_ifname" up
     ip -6 addr add "$IPV6_PREFIX:$ipv4"/64 dev "$isatap_ifname"
@@ -92,7 +99,7 @@ build_isatap_tunnel() {
 destroy_isatap_tunnel() {
     local ifname=$1
     local isatap_ifname=isa-"$ifname"
-    if ip link show | grep "$isatap_ifname" >/dev/null; then
+    if ip link show "$isatap_ifname" >/dev/null 2>&1; then
         ip link set "$isatap_ifname" down
         ip tunnel del "$isatap_ifname"
     fi
@@ -137,6 +144,34 @@ pppoe_stop() {
     [ -n "$pppd_id" ] && kill "$pppd_id" >/dev/null 2>&1
 }
 
+# Output a unused a routing table id
+# Args: none
+# e.g. get_empty_routing_table
+get_empty_routing_table() {
+    echo
+}
+
+# Configure routing table
+# Args: [ifname] [table_id] [ip address] [gateway]
+# e.g. configure_routing_table vth1 1 121.48.228.11 121.48.228.1
+configure_routing_table() {
+    local ifname=$1
+    local table_id=$2
+    local ip=$3
+    local gateway=$4
+    [ -n "$ifname" ] && [ -n "$table_id" ] && [ -n "$ip" ] && [ -n "$gateway" ] && return 1
+    ip route add default via "$gateway" dev "$ifname" table "$table_id"
+    ip rule add from "$ip" table "$table_id"
+}
+
+# Configure routing table
+# Args: [ifname]
+# e.g. delete_routing_table 1
+delete_routing_table() {
+    local table_id=$2
+
+}
+
 # Clean all dial associated with the interface
 # Args: [ifname]
 # e.g. dial_clean_all vth0
@@ -158,21 +193,9 @@ pppoe_dial() {
     if [ "$2" = "--ipv6" ]; then
         enable_ipv6=1
     fi
-    if [ -z "$ifname" ]; then
-        error "pppoe_dial: You must specify a interface"
-        exit 1
-    fi
     if [ -n "$(get_pppoe_ifname "$ifname")" ]; then
         error "pppoe_dial: There already seems to be a PPPoE connection up with $ifname"
         exit 1
-    fi
-    # The interface has not existed yet
-    if ! ip link show "$ifname" >/dev/null 2>&1; then
-        if ! create_virtual_interface "$ifname"; then
-            error "pppoe_dial: Cannot create the interface"
-            exit 1
-        fi
-        ip link set "$ifname" up
     fi
     if ! pppd plugin rp-pppoe.so "$ifname" linkname "$ifname" \
         persist hide-password noauth user "$USER" password "$PASSWORD" >/dev/null 2>&1; then
@@ -222,21 +245,9 @@ dhcp_dial() {
     if [ "$2" = "--ipv6" ]; then
         enable_ipv6=1
     fi
-    if [ -z "$ifname" ]; then
-        error "dhcp_dial: You must specify a interface"
-        exit 1
-    fi
     if [ -n "$(get_ip "$ifname" --ipv4)" ]; then
         error "dhcp_dial: There already seems to be a connection up with ($ifname)"
         exit 1
-    fi
-    # The interface has not existed yet
-    if ! ip link show "$ifname" >/dev/null 2>&1; then
-        if ! create_virtual_interface "$ifname"; then
-            error "dhcp_dial: Cannot create the interface"
-            exit 1
-        fi
-        ip link set "$ifname" up
     fi
     dhclient -nw "$ifname"
     local TIME=0
@@ -280,23 +291,11 @@ static_dial() {
     if [ "$4" = "--ipv6" ]; then
         enable_ipv6=1
     fi
-    if [ -z "$ifname" ]; then
-        error "static_dial: You must specify a interface"
-        exit 1
-    fi
     if [ -n "$(get_ip "$ifname" --ipv4)" ]; then
         error "static_dial: There already seems to be a connection up with ($ifname)"
         exit 1
     fi
     printf "Trying to create connection for %s ." "$ifname"
-    # The interface has not existed yet
-    if ! ip link show "$ifname" >/dev/null 2>&1; then
-        if ! create_virtual_interface "$ifname"; then
-            error "static_dial: Cannot create the interface"
-            exit 1
-        fi
-        ip link set "$ifname" up
-    fi
     if ip addr add "$ipv4"/"$netmask" dev "$ifname" >/dev/null 2>&1; then
         if [ "$enable_ipv6" = "1" ]; then
             if ! build_isatap_tunnel "$ifname"; then
@@ -314,46 +313,68 @@ static_dial() {
     return 1
 }
 
+# A unified dial function
+# Args: [method] [ifname] ...
+# dial_helper pppoe vth0 ...
+dial_helper() {
+    local method=$1
+    local ifname=$2
+    shift 2
+    local valid=0
+    for m in "${METHODS[@]}"; do
+        if [ "$method" = "$m" ]; then
+            valid=1
+            break
+        fi
+    done
+    if [ "$valid" != "1" ]; then
+        error "dial_helper: Method \"$method\" is not supported"
+        exit 1
+    fi
+    if [ -z "$ifname" ]; then
+        error "dial_helper: You must specify a interface"
+        exit 1
+    fi
+    # The interface has not existed yet
+    if ! ip link show "$ifname" >/dev/null 2>&1; then
+        if ! create_virtual_interface "$ifname"; then
+            error "dial_helper: Cannot create the interface"
+            exit 1
+        fi
+        ip link set "$ifname" up
+    fi
+    "${method}_dial" "$ifname" "$@"
+}
+
 # Output next virtual interface id
 # Args: none
 # e.g. get_next_vth_id
 get_next_vth_id() {
+    [ -z "$_cur_vth_id" ] && _cur_vth_id=1
     while true; do
-        ip link show "${VTH}${START_VTH_ID}" >/dev/null 2>&1 || break
-        START_VTH_ID=$((START_VTH_ID + 1))
+        ip link show "${VTH}${_cur_vth_id}" >/dev/null 2>&1 || break
+        _cur_vth_id=$((_cur_vth_id + 1))
     done
-    echo "$START_VTH_ID"
+    echo "${_cur_vth_id}"
 }
 
 # Output next static address
 # Args: none
 # e.g. get_next_address
 get_next_address() {
+    [ -z "$_cur_address" ] && _cur_address="$ADDRESS"
     while true; do
         local a=()
         local i=0
-        ip -4 addr show | grep "$START_ADDRESS" >/dev/null || break
-        for n in $(echo "$START_ADDRESS" | tr '.' ' '); do
+        ip -4 addr show | grep "$_cur_address" >/dev/null || break
+        for n in $(echo "$_cur_address" | tr '.' ' '); do
             a["$i"]=$n
             i=$((i + 1))
         done
         a[3]=$((a[3] + 1))
-        START_ADDRESS=$(echo "${a[@]}" | tr ' ' '.')
+        _cur_address=$(echo "${a[@]}" | tr ' ' '.')
     done
-    echo "$START_ADDRESS"
-}
-
-# Configure routing table
-# Args: [ifname] [table_id] [ip address] [gateway]
-# e.g. configure_routing_table vth1 1 121.48.228.11 121.48.228.1
-configure_routing_table() {
-    local ifname=$1
-    local table_id=$2
-    local ip=$3
-    local gateway=$4
-    [ -n "$ifname" ] && [ -n "$table_id" ] && [ -n "$ip" ] && [ -n "$gateway" ] && return 1
-    ip route add default via "$gateway" dev "$ifname" table "$table_id"
-    ip rule add from "$ip" table "$table_id"
+    echo "$_cur_address"
 }
 
 bulk_dial() {
@@ -408,7 +429,7 @@ bulk_dial() {
     # dhcp dial
     while [ "$dhcp_num" -gt 0 ]; do
         vth_id=$(get_next_vth_id)
-        pppoe_dial "${VTH}${vth_id}" "$enable_ipv6"
+        dial_helper "dhcp" "${VTH}${vth_id}" "$enable_ipv6"
         dhcp_num=$((dhcp_num - 1))
     done
     # static dial
@@ -416,30 +437,38 @@ bulk_dial() {
         vth_id=$(get_next_vth_id)
         ipv4=$(get_next_address)
         netmask="$NETMASK"
-        static_dial "${VTH}${vth_id}" "$ipv4" "$netmask" "$enable_ipv6"
+        dial_helper "static" "${VTH}${vth_id}" "$ipv4" "$netmask" "$enable_ipv6"
         static_num=$((static_num - 1))
     done
 }
 
 bulk_dial_clean() {
+    for i in $DATA_DIR/*; do
+        dial_clean_all "$(basename "$i")"
+    done
     for i in $(ip link show | grep -E -o 'vth[0-9]{0,3}'); do
         dial_clean_all "$i"
     done
 }
 
-case "$1" in
--i)
-    ifname=$2
-    pppoe_dial "$ifname"
-    ;;
--r)
-    ifname=$2
-    dial_clean_all "$ifname"
-    ;;
--c)
-    bulk_dial_clean
-    ;;
-*)
-    bulk_dial "$@"
-    ;;
-esac
+main() {
+    [ ! -d "$DATA_DIR" ] && mkdir -p "$DATA_DIR"
+    case "$1" in
+    -i)
+        ifname=$2
+        pppoe_dial "$ifname"
+        ;;
+    -r)
+        ifname=$2
+        dial_clean_all "$ifname"
+        ;;
+    -c)
+        bulk_dial_clean
+        ;;
+    *)
+        bulk_dial "$@"
+        ;;
+    esac
+}
+
+main "$@"
